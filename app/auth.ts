b3f5e94f56729f4240bbe854f06ac5b93f9ae7da
mazen ahmed <46138799+Mazen-Ahmed12@users.noreply.github.com>
2026-05-22 17:59:09 +0300

    .

--- app/auth.ts
+++ app/auth.ts
@@ -1,18 +1,8 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-    }),
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
     Credentials({
       credentials: {
         email: { label: "Email", type: "email" },
